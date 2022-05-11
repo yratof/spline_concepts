006e24adcaf811cf2b6c9d2feb0215e338009e0b
Andrew Lazarus <yratof@gmail.com>
2022-05-11 09:51:22 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/WR7F0Eja3dkYhgOp/scene.splinecode');
